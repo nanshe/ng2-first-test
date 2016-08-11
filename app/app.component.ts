e6a6b990582493d0eb43781f6cda1dacc9e95197
nanshe <nanshe@users.noreply.github.com>
2016-08-11 08:30:39 -0400

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Customer App';
+    name = 'Nancy';
+    newColor = 'green';
+
+    changeColor() {
+        this.newColor = this.newColor === 'green' ? 'red' : 'green';
+    }
+}
